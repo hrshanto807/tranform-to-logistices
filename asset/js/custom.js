a7a19d27abeaf2052532061522a0f75121852dbf
hrsanto <shantosss4907@gmail.com>
2024-01-08 14:43:52 +0600

    about.html done

--- asset/js/custom.js
+++ asset/js/custom.js
@@ -20,6 +20,29 @@
         autoplayTimeout: 2000,
         autoplayHoverPause: true,        
 
+    });  
+    // faqs
+
+
+    $('.single-accordian:first-child').children('.single-accordian-content').slideDown().prev('.single-accordian-tittle').addClass('active-accordian');
+
+    $('.single-accordian-tittle').click(function () {
+        $(this).toggleClass('active-accordian').next().slideToggle().parent('.single-accordian').siblings('.single-accordian').children('.single-accordian-content').hide();
+
+        $(this).parent('.single-accordian').siblings('.single-accordian').children('.single-accordian-tittle').removeClass('active-accordian')
     });
+  
 
 })(jQuery);
+ // JavaScript to handle tab switching
+ function showTab(tabId) {
+    // Hide all tab contents
+    var tabContents = document.querySelectorAll('.tab-content');
+    tabContents.forEach(function(tabContent) {
+      tabContent.style.display = 'none';
+    });
+
+    // Show the selected tab content
+    var selectedTab = document.getElementById(tabId);
+    selectedTab.style.display = 'block';
+  }
